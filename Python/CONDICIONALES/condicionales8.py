75ac138d97935172bb3c05d5911cbe8b85173f5b
Allison Restrepo <allis.restrepo@gmail.com>
2025-02-20 23:16:57 -0500

    otro ejercicio de condicionales

---
+++ Python/CONDICIONALES/condicionales8.py
@@ -0,0 +1,7 @@
+contrasena_correcta = "Python123"
+contrasena = input("Ingresa la contraseña: ")
+
+if contrasena == contrasena_correcta:
+    print("Acceso concedido.")
+else:
+    print("Contraseña incorrecta. Inténtalo de nuevo.")
